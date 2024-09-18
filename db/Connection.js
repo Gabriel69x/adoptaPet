aa688df8e5761cc8920120e7abec83122b0b7981
Gabriel69 <gabrielcortes69x@gmail.com>
2024-09-18 17:04:54 -0500

    funcionando conexion a mongo

--- db/Connection.js
+++ db/Connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-let conexion = "mongodb+srv://gabriel69x:<0000>@adoptaappcluster.ocj5g.mongodb.net/AdoptaPet?retryWrites=true&w=majority&appName=AdoptaAppCluster";
+let conexion = "mongodb+srv://gabriel69x:0000@adoptaappcluster.ocj5g.mongodb.net/AdoptaPet?retryWrites=true&w=majority&appName=AdoptaAppCluster";
 
 mongoose.connect(conexion)
 .then(event => console.log("conectado a mongo"))
