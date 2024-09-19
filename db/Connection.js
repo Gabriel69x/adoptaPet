87160afd413ccc7cc800ad914df2a1e00d8a8ef9
Gabriel69 <gabrielcortes69x@gmail.com>
2024-09-18 21:43:48 -0500

    se quito fecha por ordenes xd

--- db/Connection.js
+++ db/Connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 let conexion = "mongodb+srv://gabriel69x:0000@adoptaappcluster.ocj5g.mongodb.net/AdoptaPet?retryWrites=true&w=majority&appName=AdoptaAppCluster";
 
 mongoose.connect(conexion)
-.then(event => console.log("conectado a mongo hoy 18 de septiembre"))
+.then(event => console.log("conectado a mongo"))
 .catch(error => console.log(error)); 
 
 module.exports = mongoose;     
